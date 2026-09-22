28363a589bdd364de97c13adf6a4a9650d4b4a57
Byrd, Nicholas Pratt <nbyrd32@gatech.edu>
2026-09-22 12:40:34 -0400

    setup env and venv and add README explaining setup

---
+++ README.md
@@ -0,0 +1,27 @@
+## Prerequisites
+- Python 3.11+
+- pip
+
+## Setup Instructions
+
+1. Clone repo:
+```
+git clone https://github.com/Aryan-a-498/cs2340project1.git
+```
+
+2. Setup virtual environment (for all dependencies)
+
+```
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+3. Install dependencies:
+```
+pip install -r requirements.txt
+```
+
+4. Create .env file from template (.env.example)
+```
+cp .env.example .env
+```
--- jobless/settings.py
+++ jobless/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-28l87cfb@9fhle(mvob1-ox3s!&s8f6l(-r=aad(dt1&ky!ju-'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
