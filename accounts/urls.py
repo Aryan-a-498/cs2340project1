a2a9627f7bbbb4630c4ea1432d503f7679abb27a
Byrd, Nicholas Pratt <nbyrd32@gatech.edu>
2026-09-22 22:37:19 -0400

    add account signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('login/', views.login, name='accounts.login'),
   path('logout/', views.logout, name='accounts.logout'),
+  path('signup/', views.signup, name='accounts.signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
-from django.http import HttpResponse
+from .forms import CustomUserCreationForm
+
+def signup(request):
+    template_data = {}
+    template_data['title'] = 'Sign Up'
+    if request.method == 'GET':
+        template_data['form'] = CustomUserCreationForm()
+        return render(request, 'accounts/signup.html', {'template_data': template_data})
+    elif request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts.login')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html', {'template_data': template_data})
 
 
 def login(request):
